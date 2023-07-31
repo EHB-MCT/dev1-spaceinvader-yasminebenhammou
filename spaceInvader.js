a9a18ec0ab64072b7b87938fd49d36e14f6e52ed
yasminebenhammou <yasmine.benhammou@student.ehb.be>
2023-07-31 19:04:26 +0200

    spaceInvader tweede zit

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,6 +1,5 @@
 "use strict";
 
-alert("Draw your space invader here");
 
 drawSpaceinvader();
 
@@ -9,6 +8,7 @@ function drawSpaceinvader() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     let context = canvas.getContext("2d");
+    let colorBlocks = "#66FF00";
 
     context.beginPath();
     context.rect(50, 50, 300, 300);
@@ -18,84 +18,84 @@ function drawSpaceinvader() {
 
     context.beginPath();
     context.rect(75, 75, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = "black";
     context.fill();
 
 
     context.beginPath();
     context.rect(125, 75, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(175, 75, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(225, 75, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(275, 75, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = "black"
     context.fill();
 
     //---------
 
     context.beginPath();
     context.rect(75, 125, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
 
     context.beginPath();
     context.rect(125, 125, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(175, 125, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(225, 125, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(275, 125, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     //---------
 
     context.beginPath();
     context.rect(75, 175, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
 
     context.beginPath();
     context.rect(125, 175, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = "black"
     context.fill();
 
     context.beginPath();
     context.rect(175, 175, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(225, 175, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = "black"
     context.fill();
 
     context.beginPath();
     context.rect(275, 175, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     //----------
@@ -103,27 +103,27 @@ function drawSpaceinvader() {
 
     context.beginPath();
     context.rect(125, 225, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(225, 225, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(225, 275, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(125, 275, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
     context.beginPath();
     context.rect(175, 275, 50, 50);
-    context.fillStyle = "#00ff00";
+    context.fillStyle = colorBlocks
     context.fill();
 
 
